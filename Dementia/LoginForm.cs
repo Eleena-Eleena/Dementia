68f73fc5cfd255902d67b1498be675bf682611d9
Eleena <cuculuya.nusha@outlook.com>
2021-05-25 10:58:52 +0300

    Авторизация.

--- Dementia/LoginForm.cs
+++ Dementia/LoginForm.cs
@@ -45,8 +45,39 @@ namespace Dementia
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            
+            string ConnStr = @"Data Source=eleena\sqlexpress;Initial Catalog=Dementia;Integrated Security=True";
+            SqlConnection dbConnection = new SqlConnection(ConnStr);
+            dbConnection.Open();
 
+            bool success = false;
+            try
+            {
+                const string comand = "Select * From [Users] WHERE Login=@Login AND Password=@Password";
+                SqlCommand check = new SqlCommand(comand, dbConnection);
+
+                check.Parameters.AddWithValue("@Login", LoginField.Text);
+                check.Parameters.AddWithValue("@Password", PassField.Text);
+
+
+                using (SqlDataReader dataReader = check.ExecuteReader())
+                {
+                    success = dataReader.Read();
+                }
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+            if (success)
+            {
+                MessageBox.Show("Все верно.");
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.");
+            }
         }
+
     }
 }
